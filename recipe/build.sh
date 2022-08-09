a47e4a3f56996f28e813158fa756731c36320b46
Mike Sarahan <msarahan@gmail.com>
2022-08-08 20:46:52 -0500

    try manual target for rust on mac for Metal

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
 cargo bundle-licenses --format yaml --output DENO_THIRDPARTY_LICENSES.yml
-cargo build --release
-
-if [[ $OSTYPE == 'darwin'* ]]; then
-    # this is how we get Metal, which Deno needs
-    export CFLAGS="${CFLAGS} -isysroot ${SDKROOT:-$CONDA_BUILD_SYSROOT}"
+if [[ "$SUBDIR" =~ ^osx.* ]]; then
+    # requires special treatment because we need the Metal framework.
+    if [ "$SUBDIR" = "osx-x64" ]; then
+        cargo build --target x86_64-apple-darwin --release
+    else
+        cargo build --target aarch64-apple-darwin --release
+    fi
+else
+    cargo build --release
 fi
 
 mkdir -p $PREFIX/bin
