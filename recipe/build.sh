24fe695c4974d04ca6d6e1d02d26f45bbbf10f1e
Michael Sarahan <msarahan@gmail.com>
2022-09-06 18:02:48 -0500

    tweak build vars

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env sh
 
+cargo bundle-licenses --format yaml --output DENO_THIRDPARTY_LICENSES.yml
+# if [[ "$SUBDIR" =~ ^osx.* ]]; then
+#     if [ "$SUBDIR" = "osx-x64" ]; then
+#         export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
+#     else
+#         export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
+#     fi
+# fi
+
+# if [ "$SUBDIR" = "linux-aarch64" ]; then
+#     export CARGO_TARGET_AARCH64_LINUX_LINKER=$CC
+# fi
+
+cargo build --release
+
 mkdir -p $PREFIX/bin
-if [ "$SUBDIR" = "osx-arm64" ]; then
-    mv deno $PREFIX/bin
-else
-
-    cargo bundle-licenses --format yaml --output DENO_THIRDPARTY_LICENSES.yml
-    if [[ "$SUBDIR" =~ ^osx.* ]]; then
-        if [ "$SUBDIR" = "osx-x64" ]; then
-            export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
-        else
-            export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
-        fi
-    fi
-
-    if [ "$SUBDIR" = "linux-aarch64" ]; then
-        export CARGO_TARGET_AARCH64_LINUX_LINKER=$CC
-    fi
-
-    cargo build --release
-
-    mkdir -p $PREFIX/bin
-    OUTPUT_EXE=$(find target -name deno | tail -n 1)
-    mv $OUTPUT_EXE $PREFIX/bin/deno
-fi
+OUTPUT_EXE=$(find target -name deno | tail -n 1)
+mv $OUTPUT_EXE $PREFIX/bin/deno
 
 mkdir -p $PREFIX/etc/conda/activate.d
 echo "export DENO_INSTALL_ROOT=$PREFIX" > "${PREFIX}/etc/conda/activate.d/deno.sh"
