3b4d4570a056d1e330c043e5021d1ee8ce33e576
Michael Ekstrand <michaelekstrand@boisestate.edu>
2023-07-06 12:12:07 -0600

    exit build.bat on error

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,18 @@
 
 cargo bundle-licenses --format yaml --output DENO_THIRDPARTY_LICENSES.yml
+if errorlevel 1 exit 1
 cargo build --release
+if errorlevel 1 exit 1
+
 mkdir %LIBRARY_BIN%
+if errorlevel 1 exit 1
 COPY target\%HOST%\release\deno.exe %LIBRARY_BIN%\deno.exe
+if errorlevel 1 exit 1
 
 mkdir %PREFIX:/=\%\etc\conda\activate.d
+if errorlevel 1 exit 1
 echo SET "DENO_INSTALL_ROOT=%LIBRARY_PREFIX:/=\%" > "%PREFIX:/=\%\etc\conda\activate.d\deno.bat"
 
 mkdir %PREFIX:/=\%\etc\conda\deactivate.d
+if errorlevel 1 exit 1
 echo SET "DENO_INSTALL_ROOT=" > "%PREFIX:/=\%\etc\conda\deactivate.d\deno.bat"
