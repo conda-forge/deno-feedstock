ecaa29194e475d2c0e9d82f557f808863507f93a
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-08 09:39:09 -0500

    cross-build for aarch64 again

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,8 +31,30 @@ else
     # turn down LTO for all builds, it takes forever
     export CARGO_PROFILE_RELEASE_LTO=thin
 
+    build_args=
+    if [[ "$CONDA_TOOLCHAIN_BUILD" != "$CONDA_TOOLCHAIN_HOST" ]]; then
+        echo "Cross-compiling for target $CARGO_BUILD_TARGET" >&2
+        # echo "Building patched cargo" >&2
+        # (cd cargo-cross && cargo build --release --features all-static --target x86_64-unknown-linux-gnu)
+        # CARGO="$PWD/cargo-cross/target/x86_64-unknown-linux-gnu/release/cargo"
+
+        # we know what we're doing lol
+        export DENO_SKIP_CROSS_BUILD_CHECK=1
+        # this var screws up libffi builds, we need both build & host builds
+        unset host_alias
+
+        # # set up the cross-build things
+        # export CARGO_CROSS_BUILD_CRATES=deno_runtime:deno
+        # export CARGO_CROSS_BUILD_RS=deno_runtime/build.rs:deno/build.rs
+        # export CARGO_CROSS_BUILD_RUN="$RECIPE_DIR/cross-run.sh"
+        # the hot module reloading feature gets Deno working without snapshots.
+        build_args="--target $CONDA_BUILD_TARGET --features hmr"
+    else
+        CARGO=cargo
+    fi
+
     CARGO=cargo
-    echo "$CARGO build --release $build_args" >&2
+    echo "$CARGO build --release -v $build_args" >&2
     $CARGO build --release -v $build_args
 
     mkdir -p $PREFIX/bin
