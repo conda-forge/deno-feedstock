4417042b0ebc1da091503240dc352a4e263878c3
Michael Ekstrand <mdekstrand@drexel.edu>
2024-12-24 13:32:07 -0500

    use non-snapshotting for cross-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,24 +21,15 @@ else
     # turn down LTO for all builds, it takes forever
     export CARGO_PROFILE_RELEASE_LTO=thin
 
+    CARGO=cargo
     build_args=
     if [[ "$CONDA_TOOLCHAIN_BUILD" != "$CONDA_TOOLCHAIN_HOST" ]]; then
         echo "Building for target $CARGO_BUILD_TARGET" >&2
-        echo "Building patched cargo" >&2
-        (cd cargo-cross && cargo build --release --features all-static --target x86_64-unknown-linux-gnu)
-        CARGO="$PWD/cargo-cross/target/x86_64-unknown-linux-gnu/release/cargo"
-
+        
         # we know what we're doing lol
         export DENO_SKIP_CROSS_BUILD_CHECK=1
         # this var screws up libffi builds, we need both build & host builds
         unset host_alias
-
-        # set up the cross-build things
-        export CARGO_CROSS_BUILD_CRATES=deno_runtime:deno
-        export CARGO_CROSS_BUILD_RS=deno_runtime/build.rs:deno/build.rs
-        export CARGO_CROSS_BUILD_RUN="$RECIPE_DIR/cross-run.sh"
-    else
-        CARGO=cargo
     fi
     echo "$CARGO build --release $build_args" >&2
     $CARGO build --release -v $build_args -p deno
