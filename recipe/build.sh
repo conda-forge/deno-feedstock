037758be9cfaf3a4be20c024f0f26029f9c50eb5
Daniel Nachun <daniel.nachun@gmail.com>
2026-04-16 17:56:53 -0700

    add missing dependency

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
+export LIBCLANG_PATH=${BUILD_PREFIX}/lib
 
 cargo bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo install --no-default-features --bins --no-track --locked --root ${PREFIX} --path ./cli
